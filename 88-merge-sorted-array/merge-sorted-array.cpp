5b2cb9818ec2d39b7314c8d5b2aaf4e5cb6116a0
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-12 20:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,7 +4,6 @@ public:
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
-        vector<int> res(m+n);
         while(i>=0 && j>=0){
             if(nums1[i] <= nums2[j]) {
                 nums1[k] = nums2[j];
