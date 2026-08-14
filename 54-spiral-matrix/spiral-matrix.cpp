d1c0467bdfd3862bf26a9fee3bd25de1b6a84470
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-14 11:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (57.61%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> v;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int minr = 0, minc = 0;
+        int maxr = m-1, maxc = n-1;
+        int tne = m*n;
+        int count = 0;
+        while(minr<=maxr && minc<=maxc) {
+            for(int j=minc; j<=maxc && count<tne; j++) {
+                v.push_back(matrix[minr][j]);
+                count++;
+            }
+            minr++;
+            for(int i=minr; i<=maxr && count<tne; i++) {
+                v.push_back(matrix[i][maxc]);
+                count++;
+            }
+            maxc--;
+            for(int j=maxc; j>=minc && count<tne; j--) {
+                v.push_back(matrix[maxr][j]);
+                count++;
+            }
+            maxr--;
+            for(int i=maxr; i>=minr && count<tne; i--) {
+                v.push_back(matrix[i][minc]);
+                count++;
+            }
+            minc++;
+        }
+        return v;
+        
+    }
+};
