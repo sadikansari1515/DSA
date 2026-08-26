c6f5adcad9d20c29a2316fe7ff4b510689d6a69f
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-26 17:49:30 +0530

    Time: 6 ms (32.01%) | Memory: 23.5 MB (47.83%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<int>> rows(9,vector<int>(9,0));
-        vector<vector<int>> cols(9,vector<int>(9,0));
-        vector<vector<int>> boxes(9,vector<int>(9,0));
 
-        for(int r=0; r<9; r++) {
-            for(int c=0; c<9; c++) {
+        vector<vector<int>> rows(9, vector<int>(10, 0));
+        vector<vector<int>> cols(9, vector<int>(10, 0));
+        vector<vector<int>> boxes(9, vector<int>(10, 0));
 
-                if(board[r][c] == '.') {
+        // S: O(n^2), T: O(n^2)
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+
+                if (board[r][c] == '.') {
                     continue;
                 }
 
-                int val = board[r][c] - '1';
+                int val = board[r][c] - '0';
 
-                if(rows[r][val] == 1) {
+                if (rows[r][val] == 1) {
                     return false;
                 }
 
                 rows[r][val] = 1;
 
-                if(cols[c][val] == 1) {
+                if (cols[c][val] == 1) {
                     return false;
                 }
 
                 cols[c][val] = 1;
 
-                int boxIdx = 3 * (r/3) + (c/3);
-                
-                if(boxes[boxIdx][val] == 1) {
+                int boxIdx = 3 * (r / 3) + (c / 3);
+
+                if (boxes[boxIdx][val] == 1) {
                     return false;
                 }
+
                 boxes[boxIdx][val] = 1;
             }
         }
+
         return true;
     }
 };
