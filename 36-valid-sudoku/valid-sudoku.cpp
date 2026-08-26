fbfd46a684a6638af9f87005a462457e03acb04d
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-26 17:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (46.89%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
 
-        vector<vector<int>> rows(9, vector<int>(10, 0));
-        vector<vector<int>> cols(9, vector<int>(10, 0));
-        vector<vector<int>> boxes(9, vector<int>(10, 0));
+        vector<vector<int>> rows(9, vector<int>(9, 0));
+        vector<vector<int>> cols(9, vector<int>(9, 0));
+        vector<vector<int>> boxes(9, vector<int>(9, 0));
 
         // S: O(n^2), T: O(n^2)
 
@@ -15,7 +15,7 @@ public:
                     continue;
                 }
 
-                int val = board[r][c] - '0';
+                int val = board[r][c] - '1';
 
                 if (rows[r][val] == 1) {
                     return false;
