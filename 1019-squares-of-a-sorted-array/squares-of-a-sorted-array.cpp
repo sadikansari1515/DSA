fbf066ee5dc6ba719224c75430c301d9778a3530
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-28 19:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (66.20%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -5,8 +5,8 @@ public:
         int l = 0;
         int r = n-1;
         vector<int> v(n);
-        int val;
         for(int i=n-1; i>=0; i--) {
+            int val;
             if(abs(nums[l]) > abs(nums[r])) {
                 val = nums[l];
                 l++;
