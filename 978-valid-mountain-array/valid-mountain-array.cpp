238f15682c1632b1059614167944082be437d9a7
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-30 16:38:12 +0530

    Time: 14 ms (92.14%) | Memory: 26.3 MB (52.43%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int i = 0;
         int n = arr.size();
-
+        if(n<3) return false;
+        int i = 0;
         // Increasing part
         while (i + 1 < n && arr[i] < arr[i + 1]) {
             i++;
