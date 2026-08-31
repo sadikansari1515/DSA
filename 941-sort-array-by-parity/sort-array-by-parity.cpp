e1fa7078ad57154788caefe93bea73fc934507c0
Sadik Ansari <sadikansari8082@gmail.com>
2026-08-31 20:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (52.88%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0;
+        int j = nums.size()-1;;
+        while(i<j) {
+            if(nums[i]%2!=0 && nums[j]%2==0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+            else if(nums[i]%2==0) i++;
+            else if(nums[j]%2!=0) j--;
+        }
+        return nums;
+    }
+};
