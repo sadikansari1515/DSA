209f857091174d9b6522af6f930b51d0184fdb7e
Sadik Ansari <sadikansari8082@gmail.com>
2026-09-08 09:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(), t.end());
-        if(s==t) return true;
-        else return false;
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> m;
+        for(char c : s) {
+            m[c]++;
+        }
+
+        for(char c : t) {
+            m[c]--;
+        }
+
+        for(auto it : m) {
+            if(it.second!=0) return false;
+        }
+        return true;
     }
 };
