79ec7210a8e7ed404b6e6b58ffae9695ac3c1268
Sadik Ansari <sadikansari8082@gmail.com>
2026-09-20 20:22:57 +0530

    Time: 7 ms (66.61%) | Memory: 25.7 MB (54.08%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.cpp
@@ -0,0 +1,95 @@
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+class MyLinkedList {
+public:
+    Node* head;
+    int size;
+
+    MyLinkedList() {
+        head = nullptr;
+        size = 0;
+    }
+
+    int get(int index) {
+        if (index >= size || index < 0) {
+            return -1;
+        }
+
+        Node* curr = head;
+
+        for (int i = 0; i < index; i++) {
+            curr = curr->next;
+        }
+
+        return curr->val;
+    }
+
+    void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    void addAtIndex(int index, int val) {
+        if (index > size || index < 0) {
+            return;
+        }
+
+        Node* new_node = new Node(val);
+
+        if (index == 0) {
+            new_node->next = head;
+            head = new_node;
+        }
+        else {
+            Node* curr = head;
+
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr->next;
+            }
+
+            new_node->next = curr->next;
+            curr->next = new_node;
+        }
+
+        size++;
+    }
+
+    void deleteAtIndex(int index) {
+        if (index >= size || index < 0) {
+            return;
+        }
+
+        // Delete head
+        if (index == 0) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            size--;
+            return;
+        }
+
+        Node* curr = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            curr = curr->next;
+        }
+
+        Node* temp = curr->next;
+        curr->next = curr->next->next;
+
+        delete temp;
+        size--;
+    }
+};
