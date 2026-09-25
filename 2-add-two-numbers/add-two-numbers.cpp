3f3eec80f9f017ddecc63cf65b75b653eaea12dc
Sadik Ansari <sadikansari8082@gmail.com>
2026-09-25 10:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (12.79%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,33 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dHead = new ListNode(0);
-        ListNode* curr = dHead;
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* tail = dummyHead;
         int carry = 0;
-        while(l1 != nullptr || l2 != nullptr || carry != 0) {
-            int a = l1 != nullptr ? l1->val : 0;
-            int b = l2 != nullptr ? l2->val : 0;
-            int sum = a + b + carry;
-            carry = sum/10;
-            curr->next = new ListNode(sum%10);
-            curr = curr->next;
-            if(l1!=nullptr) {
-                l1 = l1->next;
-            }
-            if(l2!=nullptr) {
-                l2 = l2->next;
-            }
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode* newNode = new ListNode(digit);
+            tail->next = newNode;
+            tail = tail->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
         }
-        return dHead->next;
+
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };
